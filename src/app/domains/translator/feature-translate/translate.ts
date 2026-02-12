ef6819131120fdd96fc65a3913bbfd2a72a1b972
geert <geert.danhier@gmail.com>
2026-02-12 14:45:11 +0100

    feat: add detecting language

--- src/app/domains/translator/feature-translate/translate.ts
+++ src/app/domains/translator/feature-translate/translate.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/cor
 import { TranslatorForm } from '../ui-translator-form/translator-form';
 import { TranslationRequest } from '../../shared/data/translation-request';
 import { Translator } from '../../shared/data/translator/translator';
+import { LanguageOption } from '../../shared/data/language-option';
 
 @Component({
   selector: 'app-translate',
@@ -13,8 +14,14 @@ export class Translate {
   private readonly translator = inject(Translator);
 
   protected readonly translatedText = signal<string>('');
+  protected readonly detectedLanguage = signal<LanguageOption>(LanguageOption.AutoDetect);
 
   protected handleTranslation(translationRequest: TranslationRequest): void {
+    if (translationRequest.fromLanguage === LanguageOption.AutoDetect) {
+      const detectedLanguage = this.translator.detectLanguage(translationRequest.sourceText);
+      this.detectedLanguage.set(detectedLanguage);
+    }
+
     const translatedText = this.translator.translate(
       translationRequest.sourceText,
       translationRequest.toLanguage,

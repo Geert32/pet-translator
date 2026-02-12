09be9a89196311d1db4ea8c088ad4e7b019b70bd
geert <geert.danhier@gmail.com>
2026-02-12 12:48:22 +0100

    refactor: update allowed languages for auto detect

--- src/app/domains/shared/data/allowed-language-map.ts
+++ src/app/domains/shared/data/allowed-language-map.ts
@@ -4,11 +4,10 @@ export const LANGUAGE_MAP: Record<LanguageOption, LanguageOption[]> = {
   [LanguageOption.Mens]: [LanguageOption.Labrador, LanguageOption.Parkiet, LanguageOption.Papegaai],
   [LanguageOption.Labrador]: [LanguageOption.Parkiet, LanguageOption.Papegaai],
   [LanguageOption.Parkiet]: [LanguageOption.Labrador, LanguageOption.Papegaai],
-  [LanguageOption.AutoDetect]: [],
-  [LanguageOption.Papegaai]: [
+  [LanguageOption.AutoDetect]: [
     LanguageOption.Labrador,
     LanguageOption.Parkiet,
     LanguageOption.Papegaai,
-    LanguageOption.Mens,
   ],
+  [LanguageOption.Papegaai]: [LanguageOption.Labrador, LanguageOption.Parkiet, LanguageOption.Mens],
 };

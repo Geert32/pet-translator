99485647f44b3045e916961ad38b64b5a8ed2007
geert <geert.danhier@gmail.com>
2026-02-12 15:53:27 +0100

    feat: add styling pipe to handle UI

--- src/app/domains/shared/data/translator/translators.ts
+++ src/app/domains/shared/data/translator/translators.ts
@@ -3,10 +3,12 @@ import { LanguageOption } from '../language-option';
 export type TranslationStrategy = {
   translate: (text: string) => string;
   isMatch: (text: string) => boolean;
+  applyStyling: (text: string) => string;
 };
 const translateDefault: TranslationStrategy = {
   translate: (text: string): string => text,
   isMatch: (_text: string) => false,
+  applyStyling: (text: string) => text,
 };
 
 const translateLabrador: TranslationStrategy = {
@@ -20,6 +22,7 @@ const translateLabrador: TranslationStrategy = {
       .toLowerCase()
       .split(' ')
       .every((w) => w === 'woef'),
+  applyStyling: (text: string) => `<span class="text-black font-extrabold">${text}</span>`,
 };
 
 const translateParkiet: TranslationStrategy = {
@@ -31,11 +34,32 @@ const translateParkiet: TranslationStrategy = {
       .join(' ');
   },
   isMatch: (text) => /^(\s*(tjilp|piep)\s*)+$/i.test(text),
+  applyStyling: (text) => text.replace(/tjilp/gi, '<i>tjilp</i>'),
 };
 
 const translatePapegaai: TranslationStrategy = {
-  translate: (text) => `Ik praat je na: ${text}`,
+  translate: (text) =>
+    text
+      .split('.')
+      .map((line) => `Ik praat je na: ${line}`)
+      .join('.'),
   isMatch: (text) => text.startsWith('Ik praat je na: '),
+  applyStyling: (text) =>
+    text
+      .split('.')
+      .filter((line) => line.trim().length > 0)
+      .map((zin) =>
+        zin
+          .trim()
+          .split(/\s+/)
+          .map((word, i) => {
+            const colorClass = i % 2 === 0 ? 'text-yellow-400' : 'text-blue-700';
+            return `<span class="${colorClass}">${word}</span>`;
+          })
+          .join(' '),
+      )
+      .filter((line) => line.length > 0)
+      .join('<br>'),
 };
 export const TRANSLATORS: Record<LanguageOption, TranslationStrategy> = {
   [LanguageOption.Labrador]: translateLabrador,
